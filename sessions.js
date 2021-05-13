ca6fed8e707ed97787f1d96678c04b3364230815
Alan Martines <alancpmartines@hotmail.com>
2021-05-13 16:19:08 -0400

    Inicial Update

--- sessions.js
+++ sessions.js
@@ -576,7 +576,7 @@ module.exports = class Sessions {
         disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
         disableWelcome: false, // Will disable the welcoming message which appears in the beginning
         updatesLog: true, // Logs info updates automatically in terminal
-        autoClose: false, // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
+        autoClose: 0, // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
         createPathFileToken: true, //creates a folder when inserting an object in the client's browser, to work it is necessary to pass the parameters in the function create browserSessionToken
       },
       // BrowserInstance
